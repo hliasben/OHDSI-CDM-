9fc98bfba518772b9e21fac3b216426dc6262472
hliasben <hliasbenn@gmail.com>
2025-09-29 15:49:23 +0300

    Update challenge_to_person.py

--- src/main/python/transformation/challenge_to_person.py
+++ src/main/python/transformation/challenge_to_person.py
@@ -2,6 +2,22 @@
 from src.main.python.model.cdm import Person
 from datetime import datetime
 
+
+gender_map = {
+            "Male": 8507,
+            "Female": 8532
+}
+race_map = {
+    "White": 8527,
+    "Black": 8516,
+    "Asian": 8515,
+    "Other": 8522
+}
+ethnicity_map = {
+    "Hispanic": 38003563,
+    "Not_Hispanic": 38003564
+}
+
 def challenge_to_person(wrapper) -> list:
     source = wrapper.get_challenge()   # <-- εδώ πλέον παίρνουμε τα data
     records_to_insert = []
@@ -17,12 +33,10 @@ def challenge_to_person(wrapper) -> list:
         if row.get('death_date'):
             death_date = datetime.strptime(row['death_date'], "%Y-%m-%d")
 
-        # gender mapping (παράδειγμα με απλό dict)
-        gender_map = {
-            "Male": 8507,
-            "Female": 8532
-        }
+        
         gender_concept_id = gender_map.get(row['gender'], 0)
+        race_concept_id = race_map.get(row['race'], 0)
+        ethnicity_concept_id = ethnicity_map.get(row['ethnicity'], 0)
 
         record = Person(
             person_id=int(row['patient_id']),
@@ -32,8 +46,8 @@ def challenge_to_person(wrapper) -> list:
             day_of_birth=birth_date.day if birth_date else None,
             death_datetime=death_date,
             gender_concept_id=gender_concept_id,
-            race_concept_id=0,  # μπορείς να προσθέσεις mapping αν έχεις
-            ethnicity_concept_id=0,
+            race_concept_id=race_concept_id,  
+            ethnicity_concept_id=ethnicity_concept_id,
             gender_source_concept_id=0,
             race_source_concept_id=0,
             ethnicity_source_concept_id=0
